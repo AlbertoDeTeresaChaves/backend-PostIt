c754957932834aacb495113e875e5a48708e6110
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2024-10-03 15:08:19 +0200

    Creacion proyecto backend

---
+++ src/main/java/com/postIt/app/BackendPostItApplication.java
@@ -0,0 +1,13 @@
+package com.postIt.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPostItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPostItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postIt/app/BackendPostItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postIt.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPostItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
