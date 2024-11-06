f1884c8a782f377679451b976da9723e8a482804
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2024-11-06 21:43:18 +0100

    Creando userDTO para futuro

---
+++ src/main/java/com/postIt/app/models/UserDTO.java
@@ -0,0 +1,15 @@
+package com.postIt.app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserDTO {
+
+	private String username;
+	private String password;
+	
+}
