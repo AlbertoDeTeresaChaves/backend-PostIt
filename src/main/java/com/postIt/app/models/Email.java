e92aa188df3397453c9266d46f4b9d2d42b47b89
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2024-11-06 20:39:11 +0100

    Agregando lombok a las recientes models

--- src/main/java/com/postIt/app/models/Email.java
+++ src/main/java/com/postIt/app/models/Email.java
@@ -1,41 +1,16 @@
 package com.postIt.app.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Email {
 
 	private String addressee;
 	private String subject;
 	private String message;
-	
-	public Email(String addressee, String subject, String message) {
-		super();
-		this.addressee = addressee;
-		this.subject = subject;
-		this.message = message;
-	}
-
-	public String getAddressee() {
-		return addressee;
-	}
-
-	public void setAddressee(String addressee) {
-		this.addressee = addressee;
-	}
-
-	public String getSubject() {
-		return subject;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	
 	
 }
--- src/main/java/com/postIt/app/models/User.java
+++ src/main/java/com/postIt/app/models/User.java
@@ -8,7 +8,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User implements Serializable{
@@ -29,45 +33,7 @@ public class User implements Serializable{
 	
 	private Boolean enabled = false;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Boolean getEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(Boolean enabled) {
-		this.enabled = enabled;
-	}
+	
 	
 	
 }
